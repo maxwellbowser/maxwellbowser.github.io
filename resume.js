6e95fbb4323ac0a4679bfcd478135062c0fac956
maxwellbowser <panchobowz@gmail.com>
2023-10-18 18:52:38 -0700

    Adding header to embeded PDF
    Also changing path to pdf as a url (not sure if it will work??)

--- resume.js
+++ resume.js
@@ -30,7 +30,7 @@ document.addEventListener("adobe_dc_view_sdk.ready", function () {
         content: {
             /* Location of file where it is hosted */
             location: {
-                URL: "Resume.pdf",
+                URL: "maxwellbowser.github.io/Resume.pdf",
                 /*
                 If the file URL requires some additional headers, then it can be passed as follows:-
                 header: [
