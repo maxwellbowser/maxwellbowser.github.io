187c733ffbcb3144db2220a7bc7c1cd9136d885a
Ryan Bowser <107726558+maxwellbowser@users.noreply.github.com>
2023-10-18 18:44:32 -0700

    Update resume.js

--- resume.js
+++ resume.js
@@ -30,7 +30,7 @@ document.addEventListener("adobe_dc_view_sdk.ready", function () {
         content: {
             /* Location of file where it is hosted */
             location: {
-                Promise: "Resume.pdf",
+                URL: "Resume.pdf",
                 /*
                 If the file URL requires some additional headers, then it can be passed as follows:-
                 header: [
