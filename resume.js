a174413c04cc37b7ba1aac8326e194cd5df53bc3
Ryan Bowser <107726558+maxwellbowser@users.noreply.github.com>
2023-10-18 19:03:14 -0700

    Update resume.js

--- resume.js
+++ resume.js
@@ -30,7 +30,7 @@ document.addEventListener("adobe_dc_view_sdk.ready", function () {
         content: {
             /* Location of file where it is hosted */
             location: {
-                URL: "maxwellbowser.github.io/Resume.pdf",
+                URL: "https://github.com/maxwellbowser/maxwellbowser.github.io/blob/main/Resume.pdf",
                 /*
                 If the file URL requires some additional headers, then it can be passed as follows:-
                 header: [
